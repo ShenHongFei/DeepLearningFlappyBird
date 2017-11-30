f6930d77951c7b7aa5c6b8390c333022fe13116b
沈鸿飞 <shf19961008@163.com>
2017-11-30 10:39:57 +0800

    add comment & refract

--- deep_q_network.py
+++ deep_q_network.py
@@ -26,81 +26,80 @@ def show_img(img,window_name='img'):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.01)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
-    return tf.Variable(initial)
-
-def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
-
 def preprocess_img(img):
-    # RGB编码转为OpenCV的BGR编码
-    # img=cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
     # 压缩图像至80*80
     img=cv2.resize(img, (80, 80))
     # 转换为灰阶图像
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # 二值化图像
     _, img = cv2.threshold(img,1,255,cv2.THRESH_BINARY)
     return img
 
-def createNetwork():
-    # network weights
-    W_conv1 = weight_variable([8, 8, 4, 32])
-    b_conv1 = bias_variable([32])
+def create_network():
+    # 输入层 80（高）*80（宽）*4（连续4幅图像的在该位置像素值）
+    input_layer = tf.placeholder("float", [None, 80, 80, 4])
 
-    W_conv2 = weight_variable([4, 4, 32, 64])
-    b_conv2 = bias_variable([64])
+    # 根据shape生成truncated normal分布的权重张量
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev = 0.01)
+        return tf.Variable(initial)
+    
+    # 根据shape生成truncated normal分布的偏差张量
+    def bias_variable(shape):
+        initial = tf.constant(0.01, shape = shape)
+        return tf.Variable(initial)
 
-    W_conv3 = weight_variable([3, 3, 64, 64])
-    b_conv3 = bias_variable([64])
+    # 2d图像卷积
+    def conv2d(x, W, stride):
+        return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
 
-    W_fc1 = weight_variable([1600, 512])
-    b_fc1 = bias_variable([512])
-
-    W_fc2 = weight_variable([512, ACTIONS])
-    b_fc2 = bias_variable([ACTIONS])
-
-    # 输入层 80*80*4
-    s = tf.placeholder("float", [None, 80, 80, 4])
-
-    # hidden layers
-    h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
+    # 隐藏层
+    # 卷积池化层1
+    W_conv1 = weight_variable([8, 8, 4, 32]) 
+    b_conv1 = bias_variable([32]) 
+    h_conv1 = tf.nn.relu(conv2d(input_layer, W_conv1, 4) + b_conv1)
+    def max_pool_2x2(x):
+        return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
     h_pool1 = max_pool_2x2(h_conv1)
 
+    # 卷积层2
+    W_conv2 = weight_variable([4, 4, 32, 64])
+    b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
 
+    # 卷积层3
+    W_conv3 = weight_variable([3, 3, 64, 64])
+    b_conv3 = bias_variable([64])
     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-
     h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
-
+    
+    # 全连接层
+    W_fc1 = weight_variable([1600, 512])
+    b_fc1 = bias_variable([512])
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
-    # readout layer
+    # 输出层
+    W_fc2 = weight_variable([512, ACTIONS])
+    b_fc2 = bias_variable([ACTIONS])
     readout = tf.matmul(h_fc1, W_fc2) + b_fc2
 
-    return s, readout, h_fc1
+    return input_layer, readout, h_fc1
 
-def trainNetwork(s, readout, h_fc1, sess):
-    # define the cost function
+def train_network(s, readout, h_fc1, sess):
+    # 定义误差函数
     a = tf.placeholder("float", [None, ACTIONS])
     y = tf.placeholder("float", [None])
     readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices=1)
     cost = tf.reduce_mean(tf.square(y - readout_action))
+    
+    # 训练目标
     train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
     # open up a game state to communicate with emulator
     game_state = game.GameState()
 
-    # store the previous observations in replay memory
-    D = deque()
+    # 经验内存
+    mem = deque()
 
     # printing
     a_file = open("logs_" + GAME + "/readout.txt", 'w')
@@ -110,9 +109,8 @@ def trainNetwork(s, readout, h_fc1, sess):
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
     
-    # 得到初始状态，预处理图像压缩至80*80*4灰阶
+    # 得到初始状态，预处理图像压缩至80*80灰阶
     img, _, _ = game_state.frame_step(do_nothing)
-    show_img(img)
     img=preprocess_img(img)
     state = np.stack((img, img, img, img), axis=2)
 
@@ -130,7 +128,7 @@ def trainNetwork(s, readout, h_fc1, sess):
     epsilon = INITIAL_EPSILON
     t = 0
     while True:
-        # choose an action epsilon greedily
+        # choose an action epsilon-greedily
         readout_t = readout.eval(feed_dict={s : [state]})[0]
         a_t = np.zeros([ACTIONS])
         action_index = 0
@@ -145,53 +143,52 @@ def trainNetwork(s, readout, h_fc1, sess):
         else:
             a_t[0] = 1 # do nothing
 
-        # scale down epsilon
+        # 随着游戏训练进行逐渐降低epsilon
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
         # 输入所选动作并观察下一时刻状态及收益
         img, reward, terminal = game_state.frame_step(a_t)
         img=preprocess_img(img)
-        
-        
         img = np.reshape(img, (80, 80, 1))
         state_ = np.append(img, state[:, :, :3], axis=2)
 
-        # store the transition in D
-        D.append((state, a_t, reward, state_, terminal))
-        if len(D) > REPLAY_MEMORY:
-            D.popleft()
+        # 存储状态至经验内存
+        mem.append((state, a_t, reward, state_, terminal))
+        
+        if len(mem) > REPLAY_MEMORY:
+            mem.popleft()
 
         # only train if done observing
         if t > OBSERVE:
-            # sample a minibatch to train on
-            minibatch = random.sample(D, BATCH)
+            # 从经验内存中抽样作为训练样本
+            minibatch = random.sample(mem, BATCH)
 
-            # get the batch variables
-            s_j_batch = [d[0] for d in minibatch]
-            a_batch = [d[1] for d in minibatch]
-            r_batch = [d[2] for d in minibatch]
-            s_j1_batch = [d[3] for d in minibatch]
+            # 批处理变量
+            S = [d[0] for d in minibatch]
+            A = [d[1] for d in minibatch]
+            R = [d[2] for d in minibatch]
+            S_ = [d[3] for d in minibatch]
 
-            y_batch = []
-            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
+            Y = []
+            S_readout = readout.eval(feed_dict = {s : S_})
             for i in range(0, len(minibatch)):
                 terminal = minibatch[i][4]
                 # if terminal, only equals reward
                 if terminal:
-                    y_batch.append(r_batch[i])
+                    Y.append(R[i])
                 else:
-                    y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+                    Y.append(R[i] + GAMMA * np.max(S_readout[i]))
 
-            # perform gradient step
+            # 使用梯度下降算法调整网络权重
             train_step.run(feed_dict = {
-                y : y_batch,
-                a : a_batch,
-                s : s_j_batch}
+                y : Y,
+                a : A,
+                s : S}
             )
 
-        # update the old values
-        state = state_
+        # 更新状态
+        state=state_
         t += 1
 
         # save progress every 10000 iterations
@@ -222,5 +219,5 @@ def trainNetwork(s, readout, h_fc1, sess):
 
 if __name__ == "__main__":
     sess = tf.InteractiveSession()
-    s, readout, h_fc1 = createNetwork()
-    trainNetwork(s, readout, h_fc1, sess)
+    s, readout, h_fc1 = create_network()
+    train_network(s, readout, h_fc1, sess)
